ca59629db44aba2c74fdd3e14fb5116669fd96ad
Raj Singh <rajs93138@gmail.com>
2025-01-19 02:37:55 +0530

    Conversion of Datatypes

---
+++ JAVASCRIPT2/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score=true;
+
+// console.log(typeof(score));
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 or false => 0
+
+let isLoggedin="";
+console.log(Boolean(isLoggedin));
+
+//0 => false
+//1 => true
+//"" => false
+//" " => true
+//"abc" => true
+
+let someNumber=23;
+let stringNumber=String(someNumber);
+console.log(stringNumber,"dataType became:",typeof stringNumber);
+//23 => "23"
