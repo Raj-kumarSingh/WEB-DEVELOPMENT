6fd52c3a46402b77263b10ba540c177cce4fc342
Raj Singh <rajs93138@gmail.com>
2025-01-20 03:02:39 +0530

    Add conversion and comparison operation examples in JavaScript

--- JAVASCRIPT2/03_conversionOperation.js
+++ JAVASCRIPT2/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 // let score=true;
 
 // console.log(typeof(score));
@@ -21,4 +22,26 @@ console.log(Boolean(isLoggedin));
 let someNumber=23;
 let stringNumber=String(someNumber);
 console.log(stringNumber,"dataType became:",typeof stringNumber);
-//23 => "23"
+//23 => "23" */
+
+// *************************** Operation ***************************
+let value=3;
+let negvalue=-value;
+console.log(negvalue);
+
+//cosole.log(3+4); //7
+//console.log(3-4); //-1
+//console.log(3*4); //12
+//console.log(3/4); //0.75
+//console.log(3%4); //3
+//console.log(3**4); //81
+
+let str1="Raj";
+let str2=" Singh";
+console.log(str1+str2); //Raj Singh
+console.log("1"+3); //13
+console.log(1+"3"); //13
+console.log("1"+3+4); //134, it is happening because of left to right associativity,that tell us to evaluate the left side first and then right side.
+console.log(1+3+"4"); //44, it is happening because of left to right associativity,that tell us to evaluate the left side first and then right side.
+console.log(1+3*4%2); //1
+console.log((1+3)*4%2); //0
---
+++ JAVASCRIPT2/04_comparison.js
@@ -0,0 +1,26 @@
+//console.log(2>3); //false
+//console.log(2<3); //true
+//console.log(2>=3); //false
+//console.log(2<=3); //true
+//console.log(2==3); //false
+//console.log(2!=3); //true
+//console.log(2===3); //false
+//console.log(2!==3); //true
+//console.log(2=="2"); //true //it is happening because of type coercion in JavaScript and coercion is the process of converting value from one type to another.
+//console.log(2==="2"); //false
+
+console.log("2">3); //false
+console.log("2"<3); //true
+
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined==null);
+
+// === is strict equality operator and it checks the value and type of the variable.
+console.log(2===3); //false
